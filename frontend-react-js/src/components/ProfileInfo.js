f7d8df02fefcf3dc8a4795e395a5e10dcea45209
Abrham-Muche <abrham.m@code-care.pro>
2024-02-15 21:50:46 +0300

    add aws-amplify signin and signup

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,25 +3,21 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { signOut } from 'aws-amplify/auth';
 
 export default function ProfileInfo(props) {
   const [popped, setPopped] = React.useState(false);
 
+  const logOut = async () => {
+    try {
+        await signOut({ global: true });
+        window.location.href = "/"
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
   const click_pop = (event) => {
     setPopped(!popped)
-  }
-
-  const signOut = async () => {
-    console.log('signOut')
-    // [TODO] Authenication
-    Cookies.remove('user.logged_in')
-    //Cookies.remove('user.name')
-    //Cookies.remove('user.username')
-    //Cookies.remove('user.email')
-    //Cookies.remove('user.password')
-    //Cookies.remove('user.confirmation_code')
-    window.location.href = "/"
   }
 
   const classes = () => {
@@ -35,7 +31,7 @@ export default function ProfileInfo(props) {
   return (
     <div className={classes()}>
       <div className="profile-dialog">
-        <button onClick={signOut}>Sign Out</button> 
+        <button onClick={logOut}>Sign Out</button> 
       </div>
       <div className="profile-info" onClick={click_pop}>
         <div className="profile-avatar"></div>
--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,86 +1,101 @@
-import './SigninPage.css';
-import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import "./SigninPage.css";
+import React, { useEffect } from "react";
+import { ReactComponent as Logo } from "../components/svg/logo.svg";
 import { Link } from "react-router-dom";
+import { getCurrentUser } from 'aws-amplify/auth'
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
-
+import { signIn } from "aws-amplify/auth";
 export default function SigninPage() {
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
-
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errors, setErrors] = React.useState("");
+  const [cognitoErrors, setCognitoErrors] = React.useState("");
+  
+  useEffect(() => {
+    getCurrentUser().then((user) => user ? window.location.href = '/' : null )
+  }, [])
   const onsubmit = async (event) => {
+    setCognitoErrors("");
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    try {
+      signIn({ username: email, password })
+        .then((user) => {
+          console.log(user)
+          localStorage.setItem(
+            "access_token",
+            user.signInUserSession.accessToken.jwtToken
+          );
+          window.location.href = "/";
+        })
+        .catch((err) => {
+          
+          setErrors(err.message);
+          if(err.message.indexOf('UserAlreadyAuthenticatedException')) {
+            window.location.href = '/'
+          }
+          console.log("Error!", err);
+        });
+    } catch (error) {
+      if(error.type === 'UserAlreadyAuthenticatedException'){
+        window.location.href = "/";
+      }
+      if (error.code == "UserNotConfirmedException") {
+        window.location.href = "/confirm";
+      }
+      setCognitoErrors(error.message);
     }
-    return false
-  }
-
+    return false;
+  };
   const email_onchange = (event) => {
     setEmail(event.target.value);
-  }
+  };
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  if (cognitoErrors) {
+    return <div className="errors">{cognitoErrors}</div>;
   }
-
+  
   return (
     <article className="signin-article">
-      <div className='signin-info'>
-        <Logo className='logo' />
+      <div className="signin-info">
+        <Logo className="logo" />
       </div>
-      <div className='signin-wrapper'>
-        <form 
-          className='signin_form'
-          onSubmit={onsubmit}
-        >
+      <div className="signin-wrapper">
+        <form className="signin_form" onSubmit={onsubmit}>
           <h2>Sign into your Cruddur account</h2>
-          <div className='fields'>
-            <div className='field text_field username'>
+          <div className="fields">
+            <div className="field text_field username">
               <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={email_onchange} 
-              />
+              <input type="text" value={email} onChange={email_onchange} />
             </div>
-            <div className='field text_field password'>
+            <div className="field text_field password">
               <label>Password</label>
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={password_onchange}
               />
             </div>
           </div>
-          {el_errors}
-          <div className='submit'>
-            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-            <button type='submit'>Sign In</button>
+          <div style={{ backgroundColor: "rebeccapurple", padding: "10px" }}>
+            <span style={{ color: "whitesmoke" }}>{errors}</span>
           </div>
 
+          <div className="submit">
+            <Link to="/forgot" className="forgot-link">
+              Forgot Password?
+            </Link>
+            <button type="submit">Sign In</button>
+          </div>
         </form>
         <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
+          <span>Don't have an account?</span>
           <Link to="/signup">Sign up!</Link>
         </div>
       </div>
-
     </article>
   );
 }
